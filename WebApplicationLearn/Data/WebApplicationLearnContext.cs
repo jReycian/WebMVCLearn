97d35fe3e161695961b6439e503efbe0968141af
Nestor <nestor@gmail.com>
2023-02-21 15:52:23 +0800

    Initialize

---
+++ WebApplicationLearn/Data/WebApplicationLearnContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationLearn.Models;
+
+namespace WebApplicationLearn.Data
+{
+    public class WebApplicationLearnContext : DbContext
+    {
+        public WebApplicationLearnContext (DbContextOptions<WebApplicationLearnContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationLearn.Models.Movie> Movie { get; set; } = default!;
+    }
+}
